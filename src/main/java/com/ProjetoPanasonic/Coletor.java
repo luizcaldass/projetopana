b260b2d52f9c5949175cfe45c6d887554e13e8cc
luizcaldass <luizg1442@gmail.com>
2025-07-09 03:04:24 -0400

    Atualização do código de banco de Dados

--- src/main/java/com/ProjetoPanasonic/Coletor.java
+++ src/main/java/com/ProjetoPanasonic/Coletor.java
@@ -5,5 +5,5 @@ import java.util.Map;
 import java.util.List;
 
 public class Coletor {
-   
+
 }
--- src/main/java/com/ProjetoPanasonic/DataCenter.java
+++ src/main/java/com/ProjetoPanasonic/DataCenter.java
@@ -1,8 +1,11 @@
 package com.ProjetoPanasonic;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class DataCenter {
+    private Connection conexao;
     private String url;
     private String user;
     private String password;
@@ -13,9 +16,38 @@ public class DataCenter {
         this.password = "Luizgamer1@";
     }
 
-    public Connection conectar() throws Exception{
-        Connection conn = DriverManager.getConnection(url, user, password);
-        System.out.println("Conectado com sucesso");
-        return conn;
+    public DataCenter(Connection conexao){
+        this.conexao = conexao;
     }
+
+    public Connection conectar() throws Exception{
+        if (conexao == null || conexao.isClosed()){
+            Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Conectado com sucesso");
+        }
+        return conexao;
+    }
+
+    public void salvarleitura(String maquina, String tag, int valor) throws Exception{
+        String sql = "INSERT INTO DadosProd (maquina, tag, valor) VALUES (?, ?, ?)";
+        try (PreparedStatement ps = conexao.prepareStatement(sql)){
+            ps.setString(1, maquina);
+            ps.setString(2, tag);
+            ps.setInt(3, valor);
+            ps.executeUpdate();
+        }
+    }
+
+    public void desconetar(){
+        try {
+            if (conexao !=null && !conexao.isClosed()){
+                conexao.isClosed();
+                System.out.println("Conecxao encerrada com sucesso");
+            }
+        } catch (SQLException e) {
+            System.err.println("Erro de desconecxao" + e.getMessage());
+        }
+    }
+
+
 }
